bdc717fe400069d1dd778f688eefe633b6ea360b
hsjoo126 <hsjoo126@naver.com>
2024-05-20 22:55:39 +0900

    1. 포스트 작성, 조회 기능구현 완료 2. 240520 3. 게시글 조회시 좋아요순, 최신순으로 볼 수 있게 구현 완료 3-1. 테스트를 위해 작성페이지 템플릿만 만들어놓음, html은 추후 더 구현 예정.

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
+
+app_name = 'posts'
+
 urlpatterns = [
-    
+    path("", views.PostAPIView.as_view(), name="api_post"),
+    path("postcreate/", views.PostcreateView.as_view(), name="postcreate")
 ]
