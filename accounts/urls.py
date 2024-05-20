a2750257ae5d43214341dd5eefa0cc5c23d95949
park <uyeongi@naver.com>
2024-05-20 22:26:56 +0900

    asd

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('signup/', views.SignUpPageView.as_view(), name='signup'),
     path('api/signup/', views.SignUpView.as_view(), name='api_signup'),
@@ -11,4 +12,6 @@ urlpatterns = [
     path('profile/image/<int:article_id>/', views.ProfileImageView.as_view(), name='profile_image_update'),
     path('profile/<int:article_id>/change-password/', views.PasswordChangeView.as_view(), name='change-password'),
     path('profile/delete-account/<int:user_id>/', views.ProfiledeleteView.as_view(), name='delete-account'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
