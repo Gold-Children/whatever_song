46ed5acbfc111d2517e2d22a601db19f7f443188
yeonnan <145152442+yeonnan@users.noreply.github.com>
2024-05-20 13:11:24 +0900

    Initial model setup

---
+++ playlist/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.conf import settings
+
+
+class Playlist(models.Model):
+    link = models.URLField()
+    save = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='playlist')
---
+++ posts/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.conf import settings
+
+
+class Post(models.Model):
+    class category(models.TextChoices):
+        RECOMMEND = "노래 추천", "노래 추천"
+        FREE = "자유", "자유"
+
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
+    )
+    link = models.URLField()
+    image = models.ImageField(
+        upload_to="posts/post_pic/%Y/%m/%d/", blank=True, null=True
+    )
+    category = models.CharField(
+        max_length=4, blank=True, choices=category.choices, default=category.FREE
+    )
+    like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="post_likes")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
