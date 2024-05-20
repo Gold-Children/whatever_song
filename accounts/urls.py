b3ac5669b9d26d2802d559134e64e2fbfdeb344a
park <uyeongi@naver.com>
2024-05-20 16:27:42 +0900

    24.05.20(월) Profile view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    
+    path('profile/<int:article_id>',views.ProfileView.as_view(), name="detail"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from .serializers import SignupSerializer
+from .models import User
 # Create your views here.
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, username):
+        user = get_object_or_404(User, username=username)
+        serializer = SignupSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
