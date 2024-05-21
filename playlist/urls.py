0ea34e87064dfa6cb7d9be05fcf086c33a1db28d
yeonnan <145152442+yeonnan@users.noreply.github.com>
2024-05-21 17:40:11 +0900

    spotify access token 발급 및 playlist 조회 2024.05.21 1. spotify access token 발급 2. 발급 받은 token으로 spotify playlist 조회

--- playlist/urls.py
+++ playlist/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    
+    path('', views.PlaylistAPIView.as_view(), name='playlist'),
+    path('search/', views.PlaylistSearchAPIView.as_view(), name='playlist-search'),
 ]
--- playlist/views.py
+++ playlist/views.py
@@ -1,3 +1,52 @@
+import requests, base64
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Playlist, SpotifyToken
+from .serializers import PlaylistSerializer
+from WhateverSong.config import CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, TOKEN_URL
 
-# Create your views here.
+
+# 토큰 발급
+def get_access_token(refresh_token):
+    encoded = base64.b64encode(f"{CLIENT_ID}:{CLIENT_SECRET}".encode("utf-8"))
+    headers = {
+        # 'Authorization': f'Basic {encoded}',
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {
+        "grant_type": "client_credentials",
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        # 'refresh_token': REFRESH_TOKEN
+    }
+    response = requests.post(TOKEN_URL, headers=headers, data=data)
+    # print("Status Code:", response.status_code)
+    # print("Response Data:", response.json())
+    response_data = response.json()
+    return response_data.get("access_token")
+
+
+# playlist 조회
+class PlaylistAPIView(APIView):
+    def get(self, request):
+        access_token = get_access_token(request.user)
+        print(access_token)
+
+        if not access_token:
+            return Response({"error": "토큰이 유효하지 않습니다."}, status=400)
+
+        headers = {"Authorization": f"Bearer {access_token}"}
+
+        # spotify api 호출
+        spotify_api = requests.get(
+            "https://api.spotify.com/v1/browse/featured-playlists", headers=headers
+        )
+        spotify_data = spotify_api.json()
+        return Response(spotify_data, status=200)
+
+
+class PlaylistSearchAPIView(APIView):
+    def post(self, request):
+        pass
