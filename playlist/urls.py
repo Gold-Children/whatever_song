cc6416b407f2514a2712f1dbde6bde9c7dcf9f26
yeonnan <145152442+yeonnan@users.noreply.github.com>
2024-05-22 11:55:01 +0900

    찜하기 기능 2024.05.22 1. 찜하기 기능 추가

--- playlist/urls.py
+++ playlist/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PlaylistAPIView.as_view(), name='playlist'),
-    path('search/', views.PlaylistSearchAPIView.as_view(), name='playlist-search'),
+    path("", views.PlaylistAPIView.as_view(), name="playlist"),
+    path("search/", views.PlaylistSearchAPIView.as_view(), name="playlist-search"),
+    path("<int:playlist_id>/zzim/", views.PlaylistZzimAPIView.as_view(), name="playlist-zzim"),
 ]
--- playlist/views.py
+++ playlist/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import PlaylistSerializer
 from WhateverSong.config import CLIENT_ID, CLIENT_SECRET, TOKEN_URL
+from django.shortcuts import get_object_or_404
+from .models import Playlist
 
 
 # 토큰 발급
@@ -48,34 +50,56 @@ class PlaylistAPIView(APIView):
 # playlist 검색
 class PlaylistSearchAPIView(APIView):
     def get(self, request):
-        search = request.query_params.get('query', None)
+        search = request.query_params.get("query", None)
 
         # 검색어가 없는 경우 오류 응답 반환 - 프론트 구현시 해당 페이지에 머물게 변경
         if not search:
-            return Response({'error':'검색어를 입력해주세요.'}, status=400)
-        
+            return Response({"error": "검색어를 입력해주세요."}, status=400)
+
         access_token = get_access_token()
         if not access_token:
             return Response({"error": "토큰이 유효하지 않습니다."}, status=400)
-        
+
         # spotify api 호출 헤더, 검색 api에 전달할 파라미터
         headers = {"Authorization": f"Bearer {access_token}"}
-        params = {'q':search, 'type':'playlist'}
+        params = {"q": search, "type": "playlist"}
 
         # spotify api 호출
-        spotify_api = requests.get('https://api.spotify.com/v1/search', headers=headers, params=params)
-        
+        spotify_api = requests.get(
+            "https://api.spotify.com/v1/search", headers=headers, params=params
+        )
+
         spotify_data = spotify_api.json()
 
         playlists = []
-        for item in spotify_data.get('playlists', {}).get('items', []):
+        for item in spotify_data.get("playlists", {}).get("items", []):
             playlist = {
-                'name' : item['name'],    # 플레이리스트 이름
-                'link': item['external_urls']['spotify'],  # 플레이리스트 링크
-                'description': item.get('description', ''),  # 플레이리스트 설명
-                'image_url': item['images'][0]['url'] if item['images'] else None  # 플레이리스트 이미지 URL (있는 경우)
+                "name": item["name"],  # 플레이리스트 이름
+                "link": item["external_urls"]["spotify"],  # 플레이리스트 링크
+                "description": item.get("description", ""),  # 플레이리스트 설명
+                "image_url": (
+                    item["images"][0]["url"] if item["images"] else None
+                ),  # 플레이리스트 이미지 URL (있는 경우)
             }
             # 리스트에 플레이리스트 추가
             playlists.append(playlist)
 
         return Response(playlists, status=200)
+
+
+#찜 등록/제거, isauthenticated 없어서 유저 로그인 시스템이랑 결합시 추가해야함
+class PlaylistZzimAPIView(APIView):
+    def post(self, request, playlist_id):
+        playlist = get_object_or_404(Playlist, id=playlist_id)
+
+        if playlist.zzim.filter(id=request.user.id).exists():
+            playlist.zzim.remove(request.user)
+            message = "찜 목록에서 삭제했습니다."
+
+        else:
+            playlist.zzim.add(request.user)
+            message = "찜 목록에 추가했습니다."
+        
+        playlist.save()
+        serializer = PlaylistSerializer(playlist)
+        return Response({"message": message, "playlist": serializer.data}, status=status.HTTP_200_OK)
