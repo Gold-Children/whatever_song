2105b1fe7225f879b296c773e1937808cb3e7289
park <uyeongi@naver.com>
2024-05-23 16:21:42 +0900

    sad

--- playlist/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.conf import settings
-
-
-class Playlist(models.Model):
-    link = models.URLField()
-    save = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='playlist')
-    zzim = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='zzim_playlists')
