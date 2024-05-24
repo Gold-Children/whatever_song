a76b610a0dc150167e5136ffaa8264cfebbc98f3
yeonnan <145152442+yeonnan@users.noreply.github.com>
2024-05-24 20:20:11 +0900

    playlist 경로 설정, playlist 편의성 추가 2024.05.24 1. playlist html 별도의 경로로 들어와야 하는걸 기본 url로 변경 2. 검색 버튼 제거 후 엔터 누르면 playlist 검색 기능으로 작동되게 변 3. playlist 듣기 버튼 제거 후 이미지 또는 타이틀 눌렀을 때 해당 페이지로 이동되게 변경

--- playlist/urls.py
+++ playlist/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.PlaylistAPIView.as_view(), name="playlist"),
+    path("", views.PlaylistPageView.as_view(), name="playlist"),
+    path("data/", views.PlaylistDataAPIView.as_view(), name="playlist-data"),
     path("search/", views.PlaylistSearchAPIView.as_view(), name="playlist-search"),
     path("<int:playlist_id>/zzim/", views.PlaylistZzimAPIView.as_view(), name="playlist-zzim"),
-    path('playlist_html/', views.PlaylistPageView.as_view(), name='playlist-html'),
 ]
