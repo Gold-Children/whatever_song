99e8af6a1dda00741aa7c310680ba05337ff1e62
hsjoo126 <hsjoo126@naver.com>
2024-05-27 09:42:45 +0900

    게시글 디테일 페이지 html, css 완료 240527 1. css에 주석 참고 2. html에 미리 작성된 js 수정 필요 3. detail url은 pk가 아닌 /detail/로 잡아놓음, 나중에 수정 필요.

--- posts/urls.py
+++ posts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", views.PostAPIView.as_view(), name="api_post"),
     path("postcreate/", views.PostcreateView.as_view(), name="postcreate"),
     path("<int:post_id>/", views.PostDetailAPIView.as_view()),
+    path('detail/', views.PostDetailView.as_view(), name='detail'),
     path("<int:post_id>/comments/", views.PostDetailAPIView.as_view()),
     path("comments/<int:comment_id>/", views.CommentAPIView.as_view()),
 ]
