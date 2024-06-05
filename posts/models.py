3b8c3fd47be7b1684c4f220965e5104227729ccc
박진수 <jingood@bagjinsuui-MacBookAir.local>
2024-06-05 12:42:29 +0900

    posts/models.py comments content 필드를 TextField에서 CharField(max_length=500)으로 변경, 로그아웃 alert 제거 2024.06.05 1. posts/models.py 수정 -> makemigrations 필요 2. base.js 로그아웃 alert 제거

--- posts/models.py
+++ posts/models.py
@@ -29,6 +29,6 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     user_nickname = models.CharField(max_length=50)
-    content = models.TextField()
+    content = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
