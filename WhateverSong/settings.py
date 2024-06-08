aaeea34f43c1bb0fc45f111a41d21373305d1e10
hsjoo126 <hsjoo126@naver.com>
2024-06-08 23:34:33 +0900

    post list css 수정, 소셜 공유 기능 추가 240608

--- WhateverSong/settings.py
+++ WhateverSong/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = config.DJANGO_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'https://whateversong.com',
+]
 
 
 # Application definition
