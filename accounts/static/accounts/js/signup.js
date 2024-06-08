5a37f19e26ae2fdfcb65b1136b7e107182ad7c4f
박진수 <jingood@bagjinsuui-MacBookAir.local>
2024-06-09 00:33:50 +0900

    email 인증 js 위치 변경

--- accounts/static/accounts/js/signup.js
+++ accounts/static/accounts/js/signup.js
@@ -30,8 +30,44 @@ document.addEventListener('DOMContentLoaded', function () {
             formData.append('image', imageInput.files[0]);
         }
 
+        sendVerificationEmail();
+        
         // CSRF 토큰을 가져옵니다.
         const csrfToken = getCsrfToken();
+
+        function sendVerificationEmail() {
+            const email = document.getElementById('email').value;
+        
+            fetch("/api/accounts/send-verification-email/", {  // 전체 경로 확인
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRFToken": csrfToken
+                },
+                body: JSON.stringify({ email: email })
+            })
+            .then(response => response.json().then(data => {
+                if (!response.ok) {
+                    throw new Error(data.error || response.statusText);
+                }
+                return data;
+            }))
+            .then(data => {
+                if (data.message) {
+                    alert(data.message);
+                } else if (data.error) {
+                    alert(data.error);
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                if (error.message === 'No user with this email exists.') {
+                    alert('입력하신 이메일 주소로 가입된 사용자가 없습니다.');
+                } else {
+                    alert(  error.message);
+                }
+            });
+        }
 
         axios.post('/api/accounts/api/signup/', formData, {
             headers: {
