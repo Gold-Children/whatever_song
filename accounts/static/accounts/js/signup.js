e1968104e0566b4a6bcf3b9b052a8b0194d670ff
alluman <mathghost11@gmail.com>
2024-06-09 01:21:23 +0900

    배포수정 2024.06.09

--- accounts/static/accounts/js/signup.js
+++ accounts/static/accounts/js/signup.js
@@ -11,41 +11,6 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    const signupForm = document.getElementById('signup-form');
-    if (!signupForm) {
-        console.error('signup-form이 존재하지 않습니다.');
-        return;
-    }
-
-    signupForm.addEventListener('submit', function(e) {
-        e.preventDefault();
-
-        const formData = new FormData();
-        formData.append('username', document.getElementById('username').value);
-        formData.append('email', document.getElementById('email').value);
-        formData.append('password', document.getElementById('password').value);
-        formData.append('nickname', document.getElementById('nickname').value);
-        const imageInput = document.getElementById('file-input');
-        if (imageInput.files.length > 0) {
-            formData.append('image', imageInput.files[0]);
-        }
-
-        axios.post('/api/accounts/api/signup/', formData, {
-            headers: {
-                'X-CSRFToken': csrfToken,
-                'Content-Type': 'multipart/form-data' // 필수 헤더
-            }
-        })
-        .then(response => {
-            window.location.href = '/api/accounts/login/';
-        })
-        .catch(error => {
-            console.log(error);
-            console.error('회원가입에 실패했습니다.', error);
-        });
-    });
-
-    
     document.getElementById('emailvarification').addEventListener('click', function(e) {
         e.preventDefault()
         sendVerificationEmail();
@@ -84,5 +49,41 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     }
+    const signupForm = document.getElementById('signup-form');
+    if (!signupForm) {
+        console.error('signup-form이 존재하지 않습니다.');
+        return;
+    }
+
+    signupForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const formData = new FormData();
+        formData.append('username', document.getElementById('username').value);
+        formData.append('email', document.getElementById('email').value);
+        formData.append('password', document.getElementById('password').value);
+        formData.append('nickname', document.getElementById('nickname').value);
+        const imageInput = document.getElementById('file-input');
+        if (imageInput.files.length > 0) {
+            formData.append('image', imageInput.files[0]);
+        }
+
+        axios.post('/api/accounts/api/signup/', formData, {
+            headers: {
+                'X-CSRFToken': csrfToken,
+                'Content-Type': 'multipart/form-data' // 필수 헤더
+            }
+        })
+        .then(response => {
+            window.location.href = '/api/accounts/login/';
+        })
+        .catch(error => {
+            console.log(error);
+            console.error('회원가입에 실패했습니다.', error);
+        });
+    });
+
+    
+    
 });
 
