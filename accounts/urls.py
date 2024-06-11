c290fd02dad43df34fc48591853291488c6548e3
park <uyeongi@naver.com>
2024-06-11 17:35:13 +0900

    포스트 이미지 고정 24.06.11

--- accounts/urls.py
+++ accounts/urls.py
@@ -17,7 +17,4 @@ urlpatterns = [
     path('api/profile/<int:pk>/image/', views.ProfileImageView.as_view(), name='profile_image_update'),
     path('api/profile/<int:pk>/change-password/', views.PasswordChangeView.as_view(), name='change-password'),
     path('api/profile/<int:pk>/delete/', views.ProfiledeleteView.as_view(), name='profile-delete'),
-    path('verify-email/<uidb64>/<token>/', views.VerifyEmailView.as_view(), name='verify-email'),
-    path('send-verification-email/', views.SendVerificationEmailView.as_view(), name='send-verification-email'),
-    path('check-email-verified/', views.CheckEmailVerifiedView.as_view(), name='check-email-verified'),
 ]
